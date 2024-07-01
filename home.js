9f00a17c11d09bb2bb7d13fe6fced7fbe092e0b4
Mark Parinas <77998505+MRParinas@users.noreply.github.com>
2024-07-01 11:24:06 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="MRP-Capital-BadBank"
+      title="Welcome to Bad Banking"
+      text="Please use the navigation bar to move around the site."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
